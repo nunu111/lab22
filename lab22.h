dec66321b609504500bf6a768be90c7b5690a87b
nunu111 <nunu111za@hotmail.com>
2022-02-03 20:04:21 +0700

    all done

--- lab22.h
+++ lab22.h
@@ -69,6 +69,39 @@ void Unit::newTurn(){
 //Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
 /////////////////////////////////////////////////////////////////////////////////////
 
+bool Unit::isDead(){
+	if(hp <=0)return true;
+	else return false;
+}
+
+void Unit::guard(){
+	guard_on =true;
+}
+
+int Unit::beAttacked(int oppatk){
+	if(guard_on == false){
+		oppatk -= def;
+		hp = hp-oppatk;
+	}
+	else{
+		oppatk = (oppatk-def)/3;
+		hp = hp-oppatk;
+	}
+	return oppatk;
+}
+
+int Unit::attack(Unit &name){
+	return name.beAttacked(atk);
+}
+
+int Unit::heal(){
+	int healhp =rand()%21+10;
+	if (hp+healhp > hpmax){
+		healhp = healhp-(hp+healhp-hpmax);
+	}
+	hp += healhp;
+	return healhp;
+}
 
 
 void drawScene(char p_action,int p,char m_action,int m){
